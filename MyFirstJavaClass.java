f774e0fb6c5c0538684797a662d059a4e5ca0340
simeonyaniliev <49561709+simeonyaniliev@users.noreply.github.com>
2019-04-12 15:55:19 +0200

    Create MyFirstJavaClass example
    Create MyFirstJavaClass example, with main method and printing few console lines.

---
+++ MyFirstJavaClass.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+  
+		System.out.println("My first lines of code!");
+		System.out.println("Hello world!");
+		System.out.println("It is so easy!");
+    
+	}
+  
+}
